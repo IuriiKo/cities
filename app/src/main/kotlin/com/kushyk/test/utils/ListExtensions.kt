105c05c51c2932f9a471be53e7c0710c3300f212
Yurii <xaocuk1@gmail.com>
2021-12-30 18:14:48 +0200

    Added new tests Fixed searching

--- app/src/main/kotlin/com/kushyk/test/utils/ListExtensions.kt
+++ app/src/main/kotlin/com/kushyk/test/utils/ListExtensions.kt
@@ -5,19 +5,42 @@ fun <E, T> List<E>.binarySearchAll(
     comparator: Comparator<T>,
     comparison: (E) -> T
 ): List<E> {
-    val startIndex = binarySearch { comparator.compare(comparison(it), prefix) }
+    val fromIndex: Int = binarySearchFromIndex(prefix, size, comparator, comparison)
+    val toIndex = binarySearchToIndex(prefix, fromIndex, comparator, comparison)
 
-    return if (startIndex >= 0) {
-        var toIndex = startIndex
-        while (++toIndex < size) {
-            val isFailed =
-                comparator.compare(comparison(get(toIndex)), prefix) != 0
-            if (isFailed) {
-                break
-            }
-        }
-        subList(startIndex, toIndex = toIndex)
-    } else {
-        emptyList()
+    return if (fromIndex >= 0) subList(fromIndex, toIndex) else emptyList()
+}
+
+private fun <E, T> List<E>.binarySearchToIndex(
+    prefix: T,
+    fromIndex: Int,
+    comparator: Comparator<T>,
+    comparison: (E) -> T
+): Int {
+    if (fromIndex < 0) return fromIndex
+    if (fromIndex >= size) return size
+
+    val index = binarySearch(fromIndex, size) { comparator.compare(comparison(it), prefix) }
+    return if (index in 1 until size) binarySearchToIndex(
+        prefix,
+        index + 1,
+        comparator,
+        comparison
+    ) else fromIndex
+}
+
+private fun <E, T> List<E>.binarySearchFromIndex(
+    prefix: T,
+    toIndex: Int,
+    comparator: Comparator<T>,
+    comparison: (E) -> T
+): Int {
+    if (toIndex <= 0) return toIndex
+    if (toIndex > size) return -1
+    val index = binarySearch(0, toIndex) { comparator.compare(comparison(it), prefix) }
+    return when {
+        index > 0 -> binarySearchFromIndex(prefix, index, comparator, comparison)
+        index == 0 -> index
+        else -> toIndex
     }
 }
--- app/src/test/kotlin/com/kushyk/test/utils/ListExtensionsTest.kt
+++ app/src/test/kotlin/com/kushyk/test/utils/ListExtensionsTest.kt
@@ -5,6 +5,7 @@ import com.kushyk.test.data.CityDto
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.empty
+import org.hamcrest.Matchers.hasSize
 import org.junit.Rule
 import org.junit.Test
 
@@ -13,7 +14,20 @@ class ListExtensionsTest {
     val cityRepositoryRule = CityRepositoryRule()
 
     @Test
-    fun `search cities with prefix Biri`() {
+    fun `search for cities in an empty list`() {
+        val prefix = "Biri"
+
+        val cities = emptyList<CityDto>().binarySearchAll(
+            prefix,
+            suffixCaseInsensitiveComparator,
+            CityDto::name
+        )
+
+        assertThat(cities, empty())
+    }
+
+    @Test
+    fun `search for cities with prefix Biri`() {
         val prefix = "Biri"
 
         val cities = cityRepositoryRule.cities.binarySearchAll(
@@ -22,12 +36,12 @@ class ListExtensionsTest {
             CityDto::name
         )
 
-        assertThat(cities.size, `is`(4))
+        assertThat(cities.size, `is`(10))
         assertThat(cities.map(CityDto::name), everyItem(startsWith(prefix)))
     }
 
     @Test
-    fun `search cities with invalid prefix`() {
+    fun `search for cities with invalid prefix`() {
         val prefix = "prefix"
 
         val cities = cityRepositoryRule.cities.binarySearchAll(
@@ -36,11 +50,11 @@ class ListExtensionsTest {
             CityDto::name
         )
 
-        assertThat(cities , `is`(empty()))
+        assertThat(cities, `is`(empty()))
     }
 
     @Test
-    fun `search cities with empty prefix`() {
+    fun `search for cities with empty prefix`() {
         val prefix = ""
 
         val cities = cityRepositoryRule.cities.binarySearchAll(
@@ -49,6 +63,61 @@ class ListExtensionsTest {
             CityDto::name
         )
 
-        assertThat(cities , `is`(empty()))
+        assertThat(cities, `is`(empty()))
+    }
+
+    @Test
+    fun `search for cities with prefix denia`() {
+        val prefix = "denia"
+
+        val cities = cityRepositoryRule.cities.binarySearchAll(
+            prefix,
+            suffixCaseInsensitiveComparator,
+            CityDto::name
+        )
+
+        assertThat(cities, hasSize(2))
+    }
+
+    @Test
+    fun `search for cities in the beginning`() {
+        val prefix = "de"
+        val list = listOf("den", "deni", "denis", "dynis")
+
+        val cities = list.binarySearchAll(
+            prefix,
+            suffixCaseInsensitiveComparator,
+            { it }
+        )
+
+        assertThat(cities, hasSize(3))
+    }
+
+    @Test
+    fun `search for cities in the end`() {
+        val prefix = "de"
+        val list = listOf("dan", "den", "deni", "denis")
+
+        val cities = list.binarySearchAll(
+            prefix,
+            suffixCaseInsensitiveComparator,
+            { it }
+        )
+
+        assertThat(cities, hasSize(3))
+    }
+
+    @Test
+    fun `search for cities in the middle`() {
+        val prefix = "den"
+        val list = listOf("dan", "den", "deni", "denis", "develop")
+
+        val cities = list.binarySearchAll(
+            prefix,
+            suffixCaseInsensitiveComparator,
+            { it }
+        )
+
+        assertThat(cities, hasSize(3))
     }
 }
